914cf2a8e7331c78b46e75c89bdb90d227284ac5
Meeliste <meelis.teern@gmail.com>
2020-09-14 15:15:34 +0300

    Update pärast pom.xml'i

---
+++ src/main/java/com/example/Auto24Pluss/Controller/Auto24PlussApplication.java
@@ -0,0 +1,13 @@
+package com.example.Auto24Pluss.Controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auto24PlussApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auto24PlussApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Auto24Pluss/Auto24PlussApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Auto24Pluss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auto24PlussApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

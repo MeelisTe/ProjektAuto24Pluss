25c48a5fcd8e8c837a6bb3454442c816e56d3780
mirk <mirko>
2020-09-18 15:35:33 +0300

    salvestab otsingut, loeb otsingu andmeid ja kuvab html-is

--- src/main/java/com/example/Auto24Pluss/Controller/SearchSave.java
+++ src/main/java/com/example/Auto24Pluss/Controller/SearchSave.java
@@ -5,18 +5,22 @@ public class SearchSave {
     private Long userId;
 
     public String getSearchlink() {
+
         return searchlink;
     }
 
     public void setSearchlink(String searchlink) {
+
         this.searchlink = searchlink;
     }
 
     public Long getUserId() {
+
         return userId;
     }
 
     public void setUserId(Long userId) {
+
         this.userId = userId;
     }
 
